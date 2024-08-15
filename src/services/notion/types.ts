da84995e2b6a76751e02af9c6d9c7987c721e5d8
wangqing <wangqing@xmov.ai>
2024-08-15 14:09:01 +0800

    wip: notion

---
+++ src/services/notion/types.ts
@@ -0,0 +1,35 @@
+import {IBlock as NKIBlock, IBlockObject,StatusPropertyItemObjectResponse} from "@tachikomas/notion-kit"
+
+export type IBlockObjectResp = IBlockObject & {
+  children: IBlockObjectResp[]
+}
+
+export interface IBlock extends NKIBlock{
+  children: IBlock[],
+}
+
+export interface IDataItem {
+  id: string
+  name: string
+  cover: string
+  last_edited_time: string
+}
+
+export interface IPost extends IDataItem {
+  tags?: any[]
+}
+
+export type IStatus = StatusPropertyItemObjectResponse
+export type IStatusName = 'In Progress' | 'Done'
+export interface IProject extends IDataItem {
+  icon?: string
+  status?: IStatusName
+  intro?: string
+  github?: string
+  cover_url?: string
+}
+export interface ITag {
+  id: string
+  name: string
+  color: string
+}
